d4d848c04983d5e98614ef220d94829008871ca8
User <Imronbektest@gmail.com>
2022-11-18 09:40:27 +0500

    Initial commit

---
+++ src/main/java/uz/hamkorbank/appgmuzbekistan/AppGmUzbekistanApplication.java
@@ -0,0 +1,13 @@
+package uz.hamkorbank.appgmuzbekistan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGmUzbekistanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGmUzbekistanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/hamkorbank/appgmuzbekistan/AppGmUzbekistanApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.hamkorbank.appgmuzbekistan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGmUzbekistanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
